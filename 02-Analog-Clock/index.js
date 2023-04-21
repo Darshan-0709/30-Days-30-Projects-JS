0bd14f5e97d742f2463da27359b44aa17340921f
Darshan <104707176+Darshan-0709@users.noreply.github.com>
2023-04-21 19:02:20 +0530

    Update index.js

--- 02-Analog-Clock/index.js
+++ 02-Analog-Clock/index.js
@@ -1,16 +1,46 @@
-function removeTransition(e){
-    if(e.propertyName !== 'transform') return ;
-    this.classList.remove('playing');
-}
-function playSound(e) {
-    const audio = document.querySelector(`audio[data-key = '${e.keyCode}']`);
-    const key = document.querySelector(`.key[data-key = '${e.keyCode}']`);
-    if(!audio) return;
-    key.classList.add('playing'); 
-    audio.currentTime = 0;
-    audio.play(); 
+const secondHand = document.querySelector('.secondHand');
+const minuteHand = document.querySelector('.minuteHand');
+const hourHand = document.querySelector('.hourHand');
+
+function currentTime() {
+    const date = new Date();
+
+    let seconds = date.getSeconds();
+    let secondsDeg = (seconds / 60) * 360 + 90;
+    if(secondsDeg == 90) {
+        // this if statement will prevent trasition for one second so that it did't transiton from 444deg to 90deg ;)
+        secondHand.style.transitionDuration = '0s';
+    }else {
+        secondHand.style.transitionDuration = '500ms';
+    }
+    secondHand.style.transform = `rotate(${secondsDeg}deg)`
+
+    let minutes = date.getMinutes();
+    let minutesDeg = (minutes / 60) * 360 + 90;
+    if(minutesDeg == 90) {
+        minuteHand.style.transitionProperty = '0';
+    }else {
+        minuteHand.style.transitionDuration = '500ms';
+    }
+    minuteHand.style.transform = `rotate(${minutesDeg}deg)`
+    
+    let hours = date.getHours();
+    let hoursDeg = (hours / 12) * 360 + 90;
+    if(hoursDeg == 90) {
+        hourHand.style.transitionProperty = '0';
+    }else {
+        hourHand.style.transitionDuration = '500ms';
+    }
+    hourHand.style.transform = `rotate(${hoursDeg}deg)`
+    console.log(hoursDeg)
+
+
+    console.log(seconds);
 }
 
-const keys = document.querySelectorAll('.key');
-keys.forEach(key => key.addEventListener('transitionend', removeTransition))
-window.addEventListener('keydown', playSound);
+setInterval(currentTime, 1000);
+
+
+
+
+
